ced63e2b745c5f8755776fd8b2a6a1066b224eff
Cube <wieszalajakub@gmail.com>
2025-07-15 20:38:37 +0200

    navigation works

---
+++ training-z/src/app/modules/dashboard/components/header/header.component.ts
@@ -0,0 +1,94 @@
+import { Component, inject, signal } from '@angular/core';
+import { AuthDataService } from '../../../auth/services/auth-data.service';
+import { NavigationEnd, Router } from '@angular/router';
+import { Role } from '../../../auth/models/role';
+import { NavBarItem } from '../../models/nav-bar-item';
+import { filter } from 'rxjs';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {
+  private readonly authDataService = inject(AuthDataService);
+  private readonly router = inject(Router);
+
+  userRole?: Role;
+
+  readonly navBarItems = signal<NavBarItem[]>([
+    {
+      text: 'Home',
+      icon: 'pi pi-home',
+      isSelected: signal<boolean>(false),
+      route: '/dashboard',
+      alternativeRoutes: [],
+      role: undefined,
+    },
+    {
+      text: 'Workouts',
+      icon: 'pi pi-book',
+      isSelected: signal<boolean>(false),
+      route: '/dashboard/workouts',
+      alternativeRoutes: [],
+      role: Role.User,
+    },
+    {
+      text: 'Students',
+      icon: 'pi pi-users',
+      isSelected: signal<boolean>(false),
+      route: '/dashboard/students',
+      alternativeRoutes: [],
+      role: Role.Trainer,
+    },
+    {
+      text: 'Notifications',
+      icon: 'pi pi-bell',
+      isSelected: signal<boolean>(false),
+      route: '/dashboard/notifications',
+      alternativeRoutes: [],
+      role: undefined,
+    },
+    {
+      text: 'Settings',
+      icon: 'pi pi-cog',
+      isSelected: signal<boolean>(false),
+      route: '/dashboard/settings',
+      alternativeRoutes: [],
+      role: undefined,
+    },
+  ]);
+
+  constructor() {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        const route = event.urlAfterRedirects;
+
+        this.navBarItems().forEach((item, index) => {
+          if (route == item.route || item.alternativeRoutes.includes(route)) {
+            const navBarItems = this.navBarItems();
+            navBarItems.forEach((x) => x.isSelected.set(false));
+
+            navBarItems[index].isSelected.set(true);
+
+            this.navBarItems.set(navBarItems);
+          }
+        });
+      });
+  }
+
+  ngOnInit(): void {
+    this.userRole = this.authDataService.getAuthData().role;
+  }
+
+  signOut(): void {
+    this.authDataService.clearAuthData();
+    this.router.navigateByUrl('');
+  }
+
+  navigateTo(route: string): void {
+    this.router.navigateByUrl(route);
+  }
+}
--- training-z/src/app/modules/dashboard/dashboard.routes.ts
+++ training-z/src/app/modules/dashboard/dashboard.routes.ts
@@ -1,11 +1,21 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomeComponent } from './components/sub/home/home.component';
+import { SettingsComponent } from './components/sub/settings/settings.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: DashboardComponent,
     children: [
+      {
+        path: '',
+        component: HomeComponent,
+      },
+      {
+        path: 'settings',
+        component: SettingsComponent,
+      },
       {
         path: '**',
         redirectTo: '',
