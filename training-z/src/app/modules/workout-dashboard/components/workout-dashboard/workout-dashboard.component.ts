e695c4e027e31d1a14b827451db6202f9c493183
Cube <wieszalajakub@gmail.com>
2025-11-24 21:20:56 +0100

    exercise rest styling

--- training-z/src/app/modules/workout-dashboard/components/workout-dashboard/workout-dashboard.component.ts
+++ training-z/src/app/modules/workout-dashboard/components/workout-dashboard/workout-dashboard.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
 import { DividerModule } from 'primeng/divider';
+import { CheckboxModule } from 'primeng/checkbox';
+import { AppInputComponent } from '../../../common/components/app-input/app-input.component';
+import { AccordionModule } from 'primeng/accordion';
+import { AppButtonComponent } from '../../../common/components/app-button/app-button.component';
 
 @Component({
   selector: 'app-workout-dashboard',
-  imports: [DividerModule],
+  imports: [
+    DividerModule,
+    CheckboxModule,
+    AppInputComponent,
+    AccordionModule,
+    AppButtonComponent,
+  ],
   templateUrl: './workout-dashboard.component.html',
   styleUrl: './workout-dashboard.component.scss',
 })
