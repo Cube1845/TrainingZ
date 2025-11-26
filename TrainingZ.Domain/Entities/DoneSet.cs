c6058217d8ec8f43794a9cdb946c2e0dd2dcada7
Cube <wieszalajakub@gmail.com>
2025-11-26 19:06:23 +0100

    entity

---
+++ TrainingZ.Domain/Entities/DoneSet.cs
@@ -0,0 +1,18 @@
+﻿using TrainingZ.Domain.Abstract;
+
+namespace TrainingZ.Domain.Entities;
+
+public class DoneSet : BaseEntity
+{
+    public string Comment { get; set; } = string.Empty;
+    public Guid DoneExerciseId { get; set; }
+    public DoneExercise? DoneExercise { get; set; }
+
+    public DoneSet(string comment, Guid doneExerciseId, DoneExercise? doneExercise, DateTime createdAt)
+    {
+        Comment = comment;
+        DoneExerciseId = doneExerciseId;
+        DoneExercise = doneExercise;
+        CreatedAt = createdAt;
+    }
+}
---
+++ TrainingZ.Infrastructure/Persistence/Configuration/Domain/DoneSetConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TrainingZ.Domain.Entities;
+
+namespace TrainingZ.Infrastructure.Persistence.Configuration.Domain;
+
+public class DoneSetConfiguration : IEntityTypeConfiguration<DoneSet>
+{
+    public void Configure(EntityTypeBuilder<DoneSet> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.HasOne(x => x.DoneExercise).WithMany(x => x.DoneSets);
+    }
+}
