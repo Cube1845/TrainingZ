1a7845d9d9d288d86f87aa464e0903744f9850cb
Cube <wieszalajakub@gmail.com>
2026-02-25 17:18:58 +0100

    Revert "Merge branch 'main' into codex/evaluate-and-enhance-home-component"
    This reverts commit 6bb4c0cbe2523a833ec52332593def0c43681877, reversing
    changes made to 9ffebeea705263d44851f04aecdea34414333bea.

--- training-z/src/app/modules/dashboard/components/sub/home/home.component.ts
+++ training-z/src/app/modules/dashboard/components/sub/home/home.component.ts
@@ -1,17 +1,46 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { AuthDataService } from '../../../../auth/services/auth-data.service';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, of } from 'rxjs';
 import { Role } from '../../../../auth/models/role';
+import { AuthDataService } from '../../../../auth/services/auth-data.service';
+import { AppToastService } from '../../../../common/services/app-toast.service';
+import { UserService } from '../../../services/user.service';
+import { CoachingService } from '../../../services/coaching.service';
+import { WorkoutsData } from '../../../models/workouts-data';
+import { WorkoutsService } from '../../../../workout-dashboard/services/workouts.service';
+import { TagModule } from 'primeng/tag';
+
+type WeekDayInfo = {
+  label: string;
+  isToday: boolean;
+};
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [TagModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent implements OnInit {
   private readonly authDataService = inject(AuthDataService);
+  private readonly userService = inject(UserService);
+  private readonly coachingService = inject(CoachingService);
+  private readonly workoutsService = inject(WorkoutsService);
+  private readonly toastService = inject(AppToastService);
+  private readonly router = inject(Router);
+
+  readonly role = Role;
 
   userRole?: Role;
+  userName = signal<string>('');
+  studentsCount = signal<number>(0);
+  hasCoach = signal<boolean>(false);
+  coachName = signal<string>('');
+  workoutsData = signal<WorkoutsData | undefined>(undefined);
+
+  todayDayName = signal<string>('');
+  todayDateLabel = signal<string>('');
+  weekDays = signal<WeekDayInfo[]>([]);
 
   ngOnInit(): void {
     this.userRole = this.authDataService.getUserRole();
